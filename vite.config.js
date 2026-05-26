d17e789074b351951356ae7cf986eec2d142df7c
AKALABOR <110409066+AKALABOR@users.noreply.github.com>
2026-05-26 11:21:06 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://akalabor.github.io/lb7-9/',
+  base: '/lb7-9/',
 })
